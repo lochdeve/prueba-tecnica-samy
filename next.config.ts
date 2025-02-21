8903f464e250a348f3e462580e36a9d9eb2d78aa
lochdeve <cglezcano4@gmail.com>
2025-02-21 12:13:33 +0000

    fix: update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['loremflickr.com'],
+    domains: ["loremflickr.com"],
+  },
+  experimental: {
+    forceSwcTransforms: true,
   },
 };
 
